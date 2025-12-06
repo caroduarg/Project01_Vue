b614be4a55401119d7828293213bdfa24e222d82
andrea <andreacduarteg@gmail.com>
2025-12-06 17:43:54 -0400

    Movie Link

--- SobreMi/app.js
+++ SobreMi/app.js
@@ -4,15 +4,15 @@ const app = Vue.createApp({
             nombre: "Andrea",
             apellido: "Duarte",
             edad: "23",
-            peliculaFavorita: "Gladiador II",
+            vuelink: 'https://www.youtube.com/watch?v=4rgYUipGJNo',
         }
     },
     methods: {
         In5Years() {
-            return parseInt(this.edad) + 5;
+            return parseInt(this.edad) + 5; // this.edad:Accede a la propiedad 'edad', parseInt(this.edad): Convierte la edad a número entero (por si viene como texto)
         },
         changeNumber() {
-            return Math.floor(Math.random() * 100);
+            return Math.floor(Math.random() * 100); // Genera un número aleatorio entre 0 y 100, Math.floor(): Quita decimales
         }
     }
 })
